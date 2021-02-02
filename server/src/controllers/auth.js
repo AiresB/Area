0f64ca2cd4f2b2f715e7a8e5345cbded4ef646d5
AiresB <aires.baptiste@epitech.eu>
2021-02-02 12:29:38 +0100

    add login (not tested)

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -3,9 +3,25 @@ const bcrypt = require('bcrypt');
 const User = require("../models/user");
 
 exports.login = async (req, res) => {
-    //Todo
-    res.send("Login coming soon")
-}
+    User.find({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN'
+              });
+            })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
 exports.register = async (req, res) => {
     delete req.body._id;
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -13,17 +13,35 @@ user.register = async function() {
         self.id = uuidv4();
     try {
         await client.query(`INSERT INTO users(id, username, password, email, google) VALUES('${self.id}', '${self.username}', '${self.password}', '${self.email}', '${self.google}')`);
+        return self;
     } catch (e) {
         console.error(e);
+        return e;
     }
 };
 
 user.update = async function() {
     try {
         await client.query(`UPDATE users SET username = '${self.username}', password = '${self.password}', email = '${self.email}', google = '${self.google}' WHERE id = '${self.id}'`);
+        return self;
     } catch (e) {
         console.error(e);
+        return e;
     }
 };
+
+user.find = async function(email) {
+    try {
+        res = await client.query(`SELECT * FROM users WHERE email = '${email}`);
+        if (res.rowCount != 1) {
+            throw 'Email invalid';
+        }
+        self = res.rows.filter(user => {return user});
+        return self;
+    } catch (e) {
+        console.error(e);
+        return e;
+    }
+}
 
 module.exports = user
