e84e80899d3a71464830244b34717d5374e69f58
AiresB <aires.baptiste@epitech.eu>
2021-03-02 21:55:55 +0100

    add action 1

--- server/src/area/actionsFunc.js
+++ server/src/area/actionsFunc.js
@@ -7,6 +7,13 @@ const {reactionAct} = require("./reactionsFunc")
 const {actions} = require("./actions")
 const {manageGoogleAction} = require("./../Google/google_api")
 
+const its_8_action = (element) => {
+    const now = Date.now();
+    if (now.getHours() == 8 && now.getMinutes() == 0) {
+        reactionAct(element)
+    }
+}
+
 const gmail_action = (element) => {
     return manageGoogleAction(1, element);
 }
@@ -17,8 +24,9 @@ const gcalendar_action = (element) => {
 
 /*  list functions */
 exports.actFuncList = {
-    1: gmail_action,
-    2: gcalendar_action
+    1: its_8_action,
+    2: gmail_action,
+    3: gcalendar_action
 }
 
 /* action checker */
