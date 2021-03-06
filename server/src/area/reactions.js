c860cf6103b6e9a2b8de7e39549b2077a1f944fd
Adrien-Mollat <61326142+Adrien-Mollat@users.noreply.github.com>
2021-03-06 14:37:58 -0500

    Mod: modify area list to be more descriptive

--- server/src/area/reactions.js
+++ server/src/area/reactions.js
@@ -1,6 +1,8 @@
 exports.reactions = {
-    1: "Gmail",
-    2: "GCalendar",
-    3: "Youtube",
-    4: "GDrive",
+    1: "Gmail: Send an email",
+    2: "GCalendar: Create an event 1 hour later",
+    3: "Youtube: Like the TOP 1 Popular FR video",
+    4: "GDrive: Comment the last file pushed into the drive",
+    5: "Youtube: Put the TOP 1 Popular FR video in a Playlist",
+    6: "GDrive: Create an empty google doc into the drive"
 }
