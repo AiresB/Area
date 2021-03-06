edc8ad96a86e68b750d9e4a0ace5531f86c205c0
Adrien-Mollat <61326142+Adrien-Mollat@users.noreply.github.com>
2021-03-06 07:44:55 -0500

    ADD: add the creation of playlist youtube reaction

--- server/src/Google/youtube_api.js
+++ server/src/Google/youtube_api.js
@@ -33,6 +33,61 @@ const likeTop1FRVidéo = async function (auth, area) {
   });
 }
 
+const createAreaPlaylist = async function(auth) {
+  var youtube = google.youtube({version: 'v3', auth});
+  const all_playlist = await youtube.playlists.list({
+    part: 'snippet',
+    mine: true
+  });
+  for (var i = 0; i < all_playlist.length; i++) {
+    if (all_playlist.data.items[i].snippet.title == 'Area')
+      return;
+  }
+  youtube.playlists.insert({
+    part: 'snippet',
+    requestBody: {
+      snippet: {
+        title: "Area"
+      }
+    }
+  });
+}
+
+const addTop1InPlaylist = async function(auth, area) {
+  var youtube = google.youtube({version: 'v3', auth});
+  const top_videos = await youtube.videos.list({
+    auth: auth,
+    part: 'snippet,contentDetails,statistics',
+    chart : "mostPopular",
+    regionCode : "FR",
+  });
+  const TOP1_video = top_videos.data.items;
+  const lol = await createAreaPlaylist(auth);
+  const all_playlist = await youtube.playlists.list({
+    part: 'snippet',
+    mine: true
+  });
+  var areaPlaylistId = ""
+  for (var i = 0; i < all_playlist.data.items.length; i++) {
+    if (all_playlist.data.items[i].snippet.title == 'Area')
+      areaPlaylistId = all_playlist.data.items[i].id;
+  }
+  console.log(areaPlaylistId);
+  await youtube.playlistItems.insert({
+    part: 'snippet',
+    requestBody: {
+      snippet: {
+        playlistId: areaPlaylistId,
+        position: 0,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: TOP1_video[0].id
+        }
+      }
+    }
+  })
+}
+
 /**
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
@@ -61,4 +116,4 @@ const detectNewSub = async function(auth, area) {
   }
 }
 
-module.exports = {likeTop1FRVidéo, detectNewSub}
+module.exports = {likeTop1FRVidéo, detectNewSub, addTop1InPlaylist}
