7ba6c0069d6b5f011b6485991d575790604daa85
Damien Chan <damien.chan@epitech.eu>
2021-03-07 08:01:37 +0100

    MOD removing client id

--- client_web/src/components/Google/Google.js
+++ client_web/src/components/Google/Google.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const dotenv = require('dotenv');
-dotenv.config();
-const CLIENT_ID = '926573912321-i5tvvg7pcqo89ejhfko6glt4hicp4kti.apps.googleusercontent.com';
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const SCOPE = "https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/drive"
 
 class GoogleBtn extends Component {
